362a75b349a2a981607b536cbf9c8b1b75ba0fa4
Vedansh Vijaywargiya <60468275+vvHacker007@users.noreply.github.com>
2021-03-09 16:47:05 +0530

    Updated the code and the .exe file

--- Code/Notepad.py
+++ Code/Notepad.py
@@ -10,8 +10,7 @@ window = Tk()
 window.title("Notepad")
 
 # With this we will give an icon to the program and we will give a geometry to the window in the form widthxheight
-window.wm_iconbitmap("notepad_icon.ico")
-window.overrideredirect(True)
+# window.wm_iconbitmap("notepad_icon.ico")
 window.geometry("{0}x{0}+0+0".format(window.winfo_screenwidth(), window.winfo_screenheight()))
 
 # With this we will be creating a lower limit of resizing the window in the format of width,height
